dd927c28fc5d009f88ea8ddfd68c4b623c8bb90d
bmalkes <bmalkes@gmail.com>
2022-05-10 18:14:40 +0300

    feat:connect to mogodb

---
+++ src/database/mongoose.database.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+const connectToDataBase = async () => {
+    await mongoose.connect(
+        `mongodb+srv://bmalkes:${process.env.DB_PASSWORD}@budget.bvf2r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+        () => console.log("Connected to mongoDb")
+    )
+}
+
+module.exports = connectToDataBase
--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+
+const conectToDataBase = require("./database/mongoose.database")
 
 dotenv.config()
-const app = express();
 
-app.use(cors());
-app.use(express.json());
+const app = express()
+conectToDataBase()
+
+app.use(cors())
+app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.json({ msg: "Hello World!" });
-});
+    res.json({ msg: "Hello World!" })
+})
 
-app.listen(5000, () => console.log("Server is running on port 5000!"));
+app.listen(5000, () => console.log("Server is running on port 5000!"))

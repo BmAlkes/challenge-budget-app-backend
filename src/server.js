8a424998bcaae01e9985e76bf3c1bf27fee14e76
bmalkes <bmalkes@gmail.com>
2022-06-23 07:03:41 +0300

    push all deploy

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 const express = require("express")
-const cookieSession = require("cookie-session")
 const cors = require("cors")
 const dotenv = require("dotenv")
-const corns  = require ('./allowCorns')
 
 const TransactionRouter = require("./routes/transactions.routes")
 const UserRouter = require("./routes/users.routes")
@@ -12,14 +10,12 @@ const conectToDataBase = require("./database/mongoose.database")
 dotenv.config()
 const app = express()
 app.use(express.json())
-app.use(cookieSession())
 app.use(cors())
-app.use(corns)
 
 conectToDataBase()
 
 app.use("/", TransactionRouter)
 app.use("/", UserRouter)
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`listening in the ${port}`))
